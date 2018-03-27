b7ca90a86bf8b29af837065ddc64e8a1cf18e610
ibrahimyolbir <ibrahimyolbirr@gmail.com>
2018-03-27 11:32:40 +0200

    call data

---
+++ ajax.js
@@ -0,0 +1,37 @@
+$(function () {
+    $('#search-form').submit(function (e) {
+        e.preventDefault();
+        var searchTerm = $('#search-input').val();
+        getRequest(searchTerm);
+    });
+
+    function getRequest(input) {
+        var url = `http://www.omdbapi.com/?`;
+        var rules = {
+            apiKey: `e4cbae2`,
+            s: input,
+            r: `json`
+        };
+
+        $.ajax({
+            url: url,
+            type: 'GET',
+            data: rules,
+            dataType: 'json'
+
+        })
+            .done(function (done) {
+                console.log(done);
+                showResults(done.Search);
+            })
+            .fail(function (fail) {
+                console.log(fail);
+            });
+    }
+
+    function showResults(data) {
+        $.each(data, function(i,value){
+            $('#search-results').append('<p>${value.Title}</p>');
+        })
+    }
+});
---
+++ json.js
@@ -0,0 +1,47 @@
+$(function () {
+    //Länka Form
+    $("#search-form").submit(function (e) {
+        e.preventDefault();
+        var searchTerm = $("#search-input").val();
+
+        
+        getRequest(searchTerm);
+
+    });
+    //Hämta Data
+    function getRequest(input) {
+        var url = `http://www.omdbapi.com/?`;
+        var rules = {
+            apiKey : `e4cbae2`,
+            s : input,
+            r : `json`
+        };
+    $.getJSON(url,  rules, function(data) {
+        console.log(data);
+        showResults(data.Search);
+    });
+    }                                     
+    // Visa data
+    function showResults(data) {
+        $.each(data, function (i, value){ 
+        $("#search-results").append(`
+                <li> 
+                    <a class="">
+                    <img class="film-banner" src=${value.Poster} alt="Det Finns Ingen Bild">
+                    
+                    </a>
+                    <div class="right-box">
+                        <span>${value.Title}</span>
+                        <span>${value.Year}</span>
+                        <span>${value.Type}</span>
+                        <span>${value.imdbID}</span>
+                    </div>
+                </li>
+        `);
+        //$("#search-results").append(`<img src=${value.Poster}>`);
+    // for(var i = 0; i < data.length;i++) {
+        //   $("#search-results").append(`<p> ${data[i].Title}</p>`);  
+        // $("#search-results").append(`<p> ${data[i].Year}</p>`);   
+        });
+    }
+});
